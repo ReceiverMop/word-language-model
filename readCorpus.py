2c7e432cf75326ee194c6e9be3485f0d5e798872
Ron Teichner <ron.teichner@gmail.com>
2018-02-10 21:53:21 +0200

    adding some notes

--- readCorpus.py
+++ readCorpus.py
@@ -1,6 +1,7 @@
 import data
 
-
+# at ./inputSimple there should be 3 txt files: train.txt, valid.txt, test.txt
+# replace train.txt with your corpus file and see whether it is processed correctly.   
 
 
 ###############################################################################
