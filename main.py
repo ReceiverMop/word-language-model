fe0e3726a53564cea81cdbe8efccf35e87b9e6af
ira.leviant <ira.leviant@st.technion.ac.il@ml12.iem.technion.ac.il>
2018-02-18 21:16:58 +0200

    GPU adapts

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ input_files=   "../corpus/clean_wiki_new.txt,../corpus/billion_word_clean.txt,..
 #input_test=  "../corpus/example_after_2phrase.txt,../corpus/clean_wiki_new_test.txt"
 #input_files=   "/home/ira/Dropbox/IraTechnion/Patterns_Research/sp_sg/clean_corpus_english/clean_wiki_new.txt,/home/ira/Dropbox/IraTechnion/Patterns_Research/sp_sg/clean_corpus_english/billion_word_clean.txt,/home/ira/Dropbox/IraTechnion/Patterns_Research/sp_sg/clean_corpus_english/webbase_all_clean.txt,/home/ira/Dropbox/IraTechnion/Patterns_Research/sp_sg/clean_corpus_english/news_2013_clean,/home/ira/Dropbox/IraTechnion/Patterns_Research/sp_sg/clean_corpus_english/news_2012_clean" #clean without 2 phrase
 #input_files= "/home/ira/Dropbox/IraTechnion/Patterns_Research/sp_sg/clean_corpus_english/clean_wiki_new_test.txt"
-input_files = "./clean_wiki_new_test.txt"
+#input_files = "./clean_wiki_new_test.txt"
 
 print('starting loading test data')
 
